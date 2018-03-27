97894cbacd60023d644e2f10b4c091fd73c45244
infoprol <bitmatt.thelab@gmail.com>
2018-03-26 22:21:10 -0500

    bitDigs uses bitwise ops to return the binary digs as a seq of 64-bit words...  vulgar indeed...

---
+++ src/Thag/Matics/Vulgar.hs
@@ -0,0 +1,30 @@
+module Thag.Matics.Vulgar 
+( binDigs
+) where
+
+import Data.Bits
+
+
+{- 64-bit unsigned looks like the biggest numeric type that can be handled in
+ - ... oh, yeah, in a 64-bit word.  hence the name.
+ - for numbers > 2**64 -1, go fuck yourself! --}
+
+
+--newtype Natural = Word
+
+
+-- digits in order of increasing significance
+-- (for n = 111010010, [ 0, 1, 0, 0, 1, 0, 1, 1, 1 n])
+binDigs :: Word -> [Word]
+binDigs n = loop n [] where
+    loop 0 acc = 0 : acc
+    loop n acc = (n .&. 1) : loop (n `shiftR` 1) acc
+
+--loop m(n `shiftR` 1) (n .&. 1)
+
+
+
+
+
+
+
